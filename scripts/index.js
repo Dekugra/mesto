590dd25aa4cbdb763be8147b9d5e51fc640b2fb5
Dekugra <dekugra@yandex.ru>
2022-01-28 08:32:53 +0300

    починить grid, поправить profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,45 @@
-let profileOpenPopupButton = document.querySelector('.profile__edit-button')
-let popupCloseButton = document.querySelector('.popup__close')
-let popup = document.querySelector('.popup')
-let popupSubmit = document.querySelector('.popup__submit')
-let profileName = document.querySelector('.profile__title')
-let profileAbout = document.querySelector('.profile__subtitle')
-let popupUserName = document.querySelector('.popup__username')
-let popupUserAbout = document.querySelector('.popup__about')
-let elementLikeButton = document.querySelector('.element__like')
+let profileOpenPopupButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__close');
+let popup = document.querySelector('.popup');
+let popupSubmit = document.querySelector('.popup__submit');
+let popupForm = document.querySelector('.popup__form');
+let profileName = document.querySelector('.profile__title');
+let profileAbout = document.querySelector('.profile__subtitle');
+let popupUserName = document.querySelector('.popup__input_username');
+let popupUserAbout = document.querySelector('.popup__input_about');
+let elementLikeButton = document.querySelector('.element__like');
 
-function openPopup(e) {
-  popup.classList.add('popup_opened')
+function openPopup() {
+  popup.classList.add('popup_opened');
+  popupUserName.value = profileName.textContent;
+  popupUserAbout.value = profileAbout.textContent;
 }
 
-function closePopup(e) {
-  popup.classList.remove('popup_opened')
+profileOpenPopupButton.addEventListener('click', openPopup);
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
 }
+
+popupCloseButton.addEventListener('click', closePopup);
+
+function submitPopup(evt) {
+  evt.preventDefault();
+  profileName.textContent = popupUserName.value;
+  profileAbout.textContent = popupUserAbout.value;
+  closePopup();
+}
+
+popupForm.addEventListener('submit', submitPopup);
+
+// popup.addEventListener('click', function (event) {
+//   if (event.target === event.currentTarget) {
+//     closePopup()
+//   }
+// })
 
 function likedButton() {
-  elementLikeButton.classList.toggle('element__like_liked')
+  elementLikeButton.classList.toggle('element__like_liked');
 }
 
-profileOpenPopupButton.addEventListener('click', openPopup)
-popupCloseButton.addEventListener('click', closePopup)
-
-popup.addEventListener('click', function (event) {
-  if (event.target === event.currentTarget) {
-    closePopup()
-  }
-})
-
-popupSubmit.addEventListener('click', function () {
-  let inputNameLength = popupUserName.value.length
-  let inputAboutLength = popupUserAbout.value.length
-
-  if (inputNameLength > 0 && inputAboutLength > 0) {
-    closePopup()
-    profileName.textContent = popupUserName.value
-    profileAbout.textContent = popupUserAbout.value
-  }
-})
-
-elementLikeButton.addEventListener('click', likedButton)
+elementLikeButton.addEventListener('click', likedButton);
