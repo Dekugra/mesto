55ce2d65841db0a879a34c5b557e6dc301c8e951
Dekugra <dekugra@yandex.ru>
2022-03-31 20:21:45 +0300

    исправить оверлей

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,10 +1,3 @@
-function handleOverlayPopup(evt) {
-  if (evt.target.classList.contains('popup')) {
-    const openedPopup = document.querySelector('.popup_opened');
-    closePopup(openedPopup);
-  }
-}
-
 function closePopupByEscape(event) {
   if (event.key === 'Escape') {
     const openedPopup = document.querySelector('.popup_opened');
@@ -15,11 +8,9 @@ function closePopupByEscape(event) {
 export function closePopup(popup) {
   popup.classList.remove('popup_opened');
   document.removeEventListener('keydown', closePopupByEscape);
-  document.removeEventListener('mousedown', handleOverlayPopup);
 }
 
 export function openPopup(popup) {
   popup.classList.add('popup_opened');
   document.addEventListener('keydown', closePopupByEscape);
-  document.addEventListener('mousedown', handleOverlayPopup);
 }
